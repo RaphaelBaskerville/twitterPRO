5382c561dce56e6e577bb57d6171241a7a89ed61
Raphael Baskerville <raphael.baskerville@gmail.com>
2016-06-04 16:42:43 -0700

    deployed

--- public/components/app.js
+++ public/components/app.js
@@ -9,7 +9,7 @@ export default class App extends Component {
   render () {
     return (
        <div>
-          <h1>TwiDerpRoWWW</h1>
+          <h1>TwiDerpRo</h1>
           {this.props.children}
        </div>
     );
