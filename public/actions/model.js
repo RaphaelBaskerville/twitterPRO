dbcb6f965d7fbcaa937410164decc8e65a8455e2
Raphael Baskerville <raphael.baskerville@gmail.com>
2016-06-07 12:02:59 -0700

    fixes frontend routes

--- public/actions/model.js
+++ public/actions/model.js
@@ -3,7 +3,7 @@ export const POST_SUCCESS = 'POST_SUCCESS';
 export function getModel(type, options, action) {
   return dispatch => {
     console.log('in getModel', type, action);
-    return fetch('//localhost:3000/api/models/'+ type + options)
+    return fetch('/api/models/'+ type + options)
           .then(data => data.json())
           .then(json => dispatch(recModel(json, action, type)));
   };
@@ -12,9 +12,16 @@ export function createGroup(params) {
   console.log('this',arguments);
   return dispatch => {
     console.log('in getModel', params);
-    let url = '//localhost:3000/api/models/list/' + params.name + '/' + params.user;
+    let url = '/api/models/list/' + params.name + '/' + params.user;
     console.log('url',url);
-    return fetch(url, {method:'POST', headers: {}})
+    return fetch(url, { 
+      method:'POST',  
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        authorization:''
+      },
+    })
           .then(data => data.json())
           .then(json => dispatch(recModel(json, POST_SUCCESS, 'list')));
   }; 
