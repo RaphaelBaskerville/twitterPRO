27add80780cb7a971ba0db3c81d36cda8da41373
Raphael Baskerville <raphael.baskerville@gmail.com>
2016-06-07 11:59:34 -0700

    fixes twitter callback url

--- public/server/server.js
+++ public/server/server.js
@@ -48,7 +48,7 @@ app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveU
 passport.use(new TwitterStrategy({
     consumerKey: TWITTER_CONSUMER_KEY,
     consumerSecret: TWITTER_CONSUMER_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+    callbackURL: "/auth/twitter/callback"
   },
   function(token, tokenSecret, profile, cb) {
     //find the user
